7aed90e265e8e11560b5e0fe4b24af205089d9ad
LaTarisha Fountain <lfountain@njcu.edu>
2018-10-16 21:47:43 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,47 @@
-def nyc_pigeon_organizer(data)
-  pigeon_list = { }
+def nyc_pigeon_organizer(pigeon_data)
+  pigeon_list ={}
+  pigeon_data[:gender].values.flatten.each do |name|
+    pigeon_list[name] = {:color => [], :gender => [], :lives => []}
+  end
+  pigeon_values(:color, pigeon_list, pigeon_data)
+  pigeon_values(:gender, pigeon_list, pigeon_data)
+  pigeon_values(:lives, pigeon_list, pigeon_data)
+  pigeon_list
+end
+
+def pigeon_values(sym, pigeon_list, pigeon_data)
+  pigeon_list.each do |name, hash|
+    hash.each do |key, value|
+      pigeon_data[sym].each do |sym_value, names|
+        if names.include?(name)
+          unless pigeon_list[name][sym].include?(sym_value.to_s)
+            pigeon_list[name][sym] << sym_value.to_s
+          end
+        end
+      end
+    end
+  end
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   
 end
